948a78933d5439980eea10ad8f84a9ab86f56030
thenerepe <62624901+thenerepe@users.noreply.github.com>
2022-12-17 14:38:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function login() {
+  var email = document.getElementById("email").value;
+  if (email == "") {
+    alert("Lütfen Email Giriniz!");
+  }
+   else {
+    alert("Thank You for Login & You are Redirecting to Campuslife Website");
+    //Redirecting to other page or webste code or you can set your own html page.
+    window.location = "mainpage.html";
+  }
+}
